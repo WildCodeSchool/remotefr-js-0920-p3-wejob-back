604e695309ae30c718e88dc4bfa51c322dcfdbb0
Valerie TV <vtylskivincent@gmail.com>
2021-01-23 20:25:35 +0100

    converted GET roads into async methods

--- routes/candidats.js
+++ routes/candidats.js
@@ -6,28 +6,62 @@ const pool = require('../pool');
 
 const urlApiUsers = '/api/candidats/';
 
-router.get('/', (req, res) => {
-  pool.query('SELECT * FROM user_fiche', (err, results) => {
-    if (err) {
-      res.status(500).json({
-        error: err.message,
-      });
-    } else {
-      res.json(results);
-    }
-  });
+router.get('/', async (req, res) => {
+  try {
+    const [fiches, fieldFiches] = await pool.query(`
+    SELECT id, lastname, firstname, description, picture, availability, mobility, isCheck
+    FROM user_fiche`);
+    const [jobs, fieldJobs] = await pool.query(`
+    SELECT j.id AS id_job, j.name AS name_job, uj.user_id AS user_id FROM job j JOIN user_job uj ON uj.job_id=j.id`);
+    const [language, fieldLanguage] = await pool.query(`SELECT l.id AS id_lang, l.language AS lang, ul.user_id AS user_id FROM language l JOIN user_language ul ON ul.language_id=l.id`);
+    const [sectors, fieldsSector] = await pool.query(`SELECT s.id AS id_sector, s.name AS name_sector, us.user_id AS user_id FROM sector_of_activity s JOIN user_sector_of_activity us ON us.sector_of_activity_id = s.id`);
+
+    const fichesCandidats = fiches.map(f => {
+      const jobToUSer = jobs.filter(j => j.user_id === f.id);
+      const langToUSer = language.filter(l => l.user_id === f.id);
+      const sectorToUSer = sectors.filter(s => s.user_id === f.id);
+      return {
+        ...f,
+        job: jobToUSer,
+        language: langToUSer,
+        sector_of_activity: sectorToUSer,
+      }
+    })
+
+    return res.status(200).json(fichesCandidats);
+
+  } catch (error) {
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  pool.query('SELECT * FROM user_fiche WHERE id=?', req.params.id, (err, results) => {
-    if (err) {
-      res.status(500).json({
-        error: err.message,
-      });
-    } else {
-      res.json(results);
+
+
+router.get('/:id', async (req, res) => {
+  try {
+    const candidatId = req.params.id;
+    console.log("candidatId = " + candidatId);
+    const [fiche, fieldFiches] = await pool.query(`
+    SELECT id, lastname, firstname, description, diploma, cv, linkedin, youtube, picture, availability, mobility, years_of_experiment, isCheck, create_at, update_at, isOpen_to_formation
+    FROM user_fiche WHERE id = ?`, candidatId);
+    const [jobs, fieldJobs] = await pool.query(`
+    SELECT j.id AS id_job, j.name AS name_job, uj.user_id AS user_id FROM job j JOIN user_job uj ON uj.job_id=j.id WHERE uj.user_id = ?`, candidatId);
+    const [language, fieldLanguage] = await pool.query(`SELECT l.id AS id_lang, l.language AS lang, ul.user_id AS user_id FROM language l JOIN user_language ul ON ul.language_id=l.id WHERE ul.user_id = ?`, candidatId);
+    const [sectors, fieldsSector] = await pool.query(`SELECT s.id AS id_sector, s.name AS name_sector, us.user_id AS user_id FROM sector_of_activity s JOIN user_sector_of_activity us ON us.sector_of_activity_id = s.id WHERE us.user_id = ?`, candidatId);
+    const profileCandidat = {
+      fiche: fiche,
+      job: jobs,
+      language: language,
+      sector_of_activity: sectors
     }
-  });
+    return res.status(200).json(profileCandidat);
+  } catch (error) {
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
 });
 
 router.put('/:id', (req, res) => {
