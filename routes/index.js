426e12ae30c42e0bd775a5aefea6971cb396205f
Valerie TV <vtylskivincent@gmail.com>
2021-01-24 14:53:33 +0100

    added user road

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const candidats = require('./candidats');
 const authRouter = require('./auth');
+const user = require('./user');
 
 const router = express.Router();
 
 router.use('/candidats', candidats);
 router.use('/auth', authRouter);
+router.use('/user', user);
 
 module.exports = router;
