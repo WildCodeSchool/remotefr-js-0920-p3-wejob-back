e1286712cf335fecd88e14eef258f0164263e979
Valerie TV <vtylskivincent@gmail.com>
2021-01-29 20:08:39 +0100

    formatting email that will be sent to candidat

--- services/send-token.js
+++ services/send-token.js
@@ -5,7 +5,8 @@ async function sendToken(recipient, token) {
   const emailBase64 = emailBuffer.toString('base64');
   const link = `${process.env.FRONT_URL}/createAnAccount?token=${token}&email=${emailBase64}`;
   const text = `Merci de confirmer votre adresse e-mail en cliquant ici : ${link}`;
-  const html = `<b><a href="${link}">Merci de confirmer votre adresse e-mail en cliquant ici.</a></b>`;
+  const html = `<p>Bonjour,<br/><br/>Vous venez d'être inscrit sur we-job.com.
+  <br/><b>Merci de confirmer votre adresse e-mail <a href="${link}">en cliquant ici.</a></b><br/><br/>À très bientôt,<br/><br/>Votre responsable We-job</p>`;
   const subject = "Confirmez votre adresse e-mail";
 
   return mailer(recipient, subject, text, html);
