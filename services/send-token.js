c5227309137125a64577850d4d540a03b098cfd5
Valerie TV <vtylskivincent@gmail.com>
2021-01-30 19:54:21 +0100

    layout the email for creation of an account

--- services/send-token.js
+++ services/send-token.js
@@ -1,13 +1,15 @@
 const mailer = require('./mailer');
+const getTemplate = require('./get-template');
 
 async function sendToken(recipient, token) {
+  const template = getTemplate('create-first-password');
   const emailBuffer = Buffer.from(recipient, 'utf-8');
   const emailBase64 = emailBuffer.toString('base64');
   const link = `${process.env.FRONT_URL}/createAnAccount?token=${token}&email=${emailBase64}`;
-  const text = `Merci de confirmer votre adresse e-mail en cliquant ici : ${link}`;
-  const html = `<p>Bonjour,<br/><br/>Vous venez d'être inscrit sur we-job.com.
-  <br/><b>Merci de confirmer votre adresse e-mail <a href="${link}">en cliquant ici.</a></b><br/><br/>À très bientôt,<br/><br/>Votre responsable We-job</p>`;
-  const subject = "Confirmez votre adresse e-mail";
+  const publicURL = process.env.PUBLIC_URL;
+  const text = `Vous avez désormais un compte We-Job. Merci de confirmer votre adresse e-mail en cliquant ici : ${link}`;
+  const html = template({ link, publicURL });
+  const subject = 'Confirmez votre adresse e-mail';
 
   return mailer(recipient, subject, text, html);
 }
