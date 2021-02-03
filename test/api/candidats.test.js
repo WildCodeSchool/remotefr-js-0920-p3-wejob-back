739739b2be107902953481d46ff74ad3c2831713
Benoît Hubert <benoithubert@gmail.com>
2021-02-03 17:00:55 +0100

    Upload files with supertest

--- test/api/candidats.test.js
+++ test/api/candidats.test.js
@@ -1,8 +1,9 @@
 const request = require('supertest');
 const { expect } = require('chai');
+const { resolve } = require('path');
 const app = require('../../app');
 const { reset: resetDatabase } = require('../db');
-const { createUser, createAndLogin } = require('../utils');
+const { createUser, createAndLogin, getUserFiche } = require('../utils');
 
 function testUpdate(...args) {
   expect(args.length).to.equal(4, 'testUpdate expects 4 arguments');
@@ -15,65 +16,66 @@ function testUpdate(...args) {
   // .expect('Content-Type', /json/)
   return req.expect(expectedStatus);
 }
+function testUpdateFiles(...args) {
+  expect(args.length).to.equal(4, 'testUpdateFiles expects 4 arguments');
+  const [userId, jwt, expectedStatus] = args;
+  return request(app)
+    .post(`/api/candidats/${userId}/files`)
+    .attach('picture', resolve(__dirname, '..', 'cat.png'))
+    .set('Accept', 'application/json')
+    .set('Cookie', [`token=${jwt}`])
+    .expect(expectedStatus);
+}
 
-describe('PUT /api/auth/candidat/:id', () => {
-  beforeEach(resetDatabase);
+describe('Candidate routes', () => {
+  describe('PUT /api/auth/candidat/:id', () => {
+    beforeEach(resetDatabase);
 
-  it('without auth', async () => {
-    const { id } = await createUser('update1@wj.com');
-    return testUpdate(id, null, {}, 401).catch((err) =>
-      console.error('in test', err),
-    );
+    it('without auth', async () => {
+      const { id } = await createUser('update1@wj.com');
+      return testUpdate(id, null, {}, 401).catch((err) =>
+        console.error('in test', err),
+      );
+    });
+
+    it('with wrong auth (other user)', async () => {
+      const { token: token1 } = await createAndLogin('update2a@wj.com');
+      const { id: id2 } = await createAndLogin('update2b@wj.com');
+      return testUpdate(id2, token1, {}, 403);
+    });
+
+    it('with empty payload (civility missing)', async () => {
+      const { id, token } = await createAndLogin('update3@wj.com');
+      return testUpdate(id, token, {}, 400);
+    });
+
+    it('with only civility', async () => {
+      const { id, token } = await createAndLogin('update4@wj.com');
+      const payload = { civility: 'Monsieur' };
+      return testUpdate(id, token, payload, 204);
+    });
+
+    it('with isCheck, as a candidate', async () => {
+      const { id, token } = await createAndLogin('update5@wj.com');
+      const payload = { civility: 'Monsieur', isCheck: 1 };
+      await testUpdate(id, token, payload, 204);
+      const fiche = await getUserFiche(id);
+      expect(fiche.isCheck).to.equal(0);
+    });
   });
 
-  it('with wrong auth (other user)', async () => {
-    const { token: token1 } = await createAndLogin('update2a@wj.com');
-    const { id: id2 } = await createAndLogin('update2b@wj.com');
-    return testUpdate(id2, token1, {}, 403);
+  describe('PUT /api/auth/candidat/:id/files', () => {
+    it('send a file', async () => {
+      const { id, token } = await createAndLogin('update6@wj.com');
+      const payload = {
+        civility: 'Monsieur',
+        firstname: 'foo',
+        lastname: 'baz',
+      };
+      await testUpdate(id, token, payload, 204);
+      await testUpdateFiles(id, token, {}, 204);
+      const fiche = await getUserFiche(id);
+      expect(fiche.isCheck).to.equal(0);
+    });
   });
-
-  it('with empty payload (civility missing)', async () => {
-    const { id, token } = await createAndLogin('update3@wj.com');
-    return testUpdate(id, token, {}, 400);
-  });
-
-  it('with only civility', async () => {
-    const { id, token } = await createAndLogin('update3@wj.com');
-    const payload = { civility: 'Monsieur' };
-    return testUpdate(id, token, payload, 204);
-  });
-
-  // it('empty payload', async () => {
-  //   const user = await createUser('update1@wj.com');
-  //   return testUpdate(user.id, null, {}, 422);
-  // });
-
-  // it('send email&password but empty', (done) => {
-  //   testLogin({ email: '', password: '' }, 422, done);
-  // });
-
-  // it('send invalid email', (done) => {
-  //   testLogin({ email: 'foobar@foobar', password: 'SomePass' }, 422, done);
-  // });
-
-  // // tester enregistrement d'email en doublon, censé renvoyer 409
-  // it('send sending same email twice', (done) => {
-  //   testLogin(
-  //     { email: 'foobar@example.com', password: 'SomePass' },
-  //     201,
-  //     () => {
-  //       testLogin(
-  //         { email: 'foobar@example.com', password: 'SomePass' },
-  //         409,
-  //         done,
-  //       );
-  //     },
-  //   );
-  // });
-
-  // tester le cas où on envoie les bons champs
-  // it('send correct fields', async () => {
-  //   await createUser('foo3@bar.com');
-  //   return testLogin({ email: 'foo3@bar.com', password: 'Zyx765**' }, 200);
-  // });
 });
--- test/utils.js
+++ test/utils.js
@@ -18,6 +18,17 @@ const getUser = async (idOrEmail) => {
   return { ...user };
 };
 
+const getUserFiche = async (userId) => {
+  const [
+    userFiche,
+  ] = await query(
+    'SELECT u.id as user_id, u.email, uf.* FROM user u JOIN user_fiche uf ON u.id = uf.user_id WHERE u.id = ?',
+    [userId],
+  );
+  const { user_id: id, id: ficheId, ...rest } = userFiche;
+  return { ...id, ficheId, ...rest };
+};
+
 const createUser = async (email, isAdmin = false) => {
   const password = await bcrypt.hash('Zyx765**', 10);
   const { insertId } = await query('INSERT INTO user SET ?', [
@@ -43,4 +54,5 @@ module.exports = {
   createUser,
   login,
   createAndLogin,
+  getUserFiche,
 };
