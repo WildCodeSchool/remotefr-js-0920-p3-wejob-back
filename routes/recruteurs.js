4bcba23951d071e9545c9f04812d89f5250de75f
Benoît Hubert <benoithubert@gmail.com>
2021-02-07 16:13:34 +0100

    Create or update recruiter entry, add visit timestamp

--- routes/recruteurs.js
+++ routes/recruteurs.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const sendRecruiterData = require('../services/send-recruiter-data');
+const pool = require('../pool');
+const { checkIsAdmin } = require('../middlewares/auth');
 
 const router = express.Router();
-
-const pool = require('../pool');
 
 router.post('/', async (req, res) => {
   try {
@@ -14,14 +14,39 @@ router.post('/', async (req, res) => {
     await sendRecruiterData(name, phone, email);
     const [
       recruiter,
-    ] = await pool.query(`SELECT id FROM recruiter WHERE email = ?`, [email]);
+    ] = await pool.query(`SELECT id, visits FROM recruiter WHERE email = ?`, [
+      email,
+    ]);
     if (recruiter.length === 0) {
+      const visits = JSON.stringify([Date.now()]);
       await pool.query(
-        `INSERT INTO recruiter(name, telephone, email) VALUES(?, ?, ?)`,
-        [name, phone, email],
+        `INSERT INTO recruiter(name, telephone, email, visits) VALUES(?, ?, ?, ?)`,
+        [name, phone, email, visits],
       );
+      return res.sendStatus(201);
     }
-    return res.sendStatus(201);
+    const { visits: previousVisits } = recruiter[0];
+    const visits = previousVisits
+      ? JSON.stringify([...previousVisits, Date.now()])
+      : JSON.stringify([Date.now()]);
+    await pool.query('UPDATE recruiter SET visits = ? WHERE email = ?', [
+      visits,
+      email,
+    ]);
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+router.get('/', checkIsAdmin, async (req, res) => {
+  try {
+    const [recruiters] = await pool.query(
+      'SELECT id, name, email, telephone, visits FROM recruiter',
+    );
+    return res.json(recruiters);
   } catch (error) {
     return res.status(500).json({
       error: error.message,
