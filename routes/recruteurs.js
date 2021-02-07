fc5d482cc511c9a6a91d5c06fbd9cdbf07ed31a7
Benoît Hubert <benoithubert@gmail.com>
2021-02-07 17:32:48 +0100

    Store latest visit first

--- routes/recruteurs.js
+++ routes/recruteurs.js
@@ -1,27 +1,45 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const sendRecruiterData = require('../services/send-recruiter-data');
+const pool = require('../pool');
+const { checkIsAdmin } = require('../middlewares/auth');
 
 const router = express.Router();
 
-const pool = require('../pool');
+const RECRUITER_COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 7;
 
 router.post('/', async (req, res) => {
   try {
     const { name, email, phone } = req.body;
-    res.cookie('recognizeRecruiter', 'true', {
-      maxAge: 1000 * 60 * 60 * 24 * 7,
+    const token = await jwt.sign({ email }, process.env.JWT_SECRET, {
+      expiresIn: '1w',
+    });
+    res.cookie('recruiter', token, {
+      maxAge: RECRUITER_COOKIE_MAX_AGE,
     });
     await sendRecruiterData(name, phone, email);
     const [
       recruiter,
-    ] = await pool.query(`SELECT id FROM recruiter WHERE email = ?`, [email]);
+    ] = await pool.query(`SELECT id, visits FROM recruiter WHERE email = ?`, [
+      email,
+    ]);
     if (recruiter.length === 0) {
+      const visits = JSON.stringify([Date.now()]);
       await pool.query(
-        `INSERT INTO recruiter(name, telephone, email) VALUES(?, ?, ?)`,
-        [name, phone, email],
+        `INSERT INTO recruiter(name, telephone, email, visits) VALUES(?, ?, ?, ?)`,
+        [name, phone, email, visits],
       );
+      return res.sendStatus(201);
     }
-    return res.sendStatus(201);
+    const { visits: previousVisits } = recruiter[0];
+    const visits = previousVisits
+      ? JSON.stringify([Date.now(), ...previousVisits])
+      : JSON.stringify([Date.now()]);
+    await pool.query('UPDATE recruiter SET visits = ? WHERE email = ?', [
+      visits,
+      email,
+    ]);
+    return res.sendStatus(204);
   } catch (error) {
     return res.status(500).json({
       error: error.message,
@@ -29,10 +47,32 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.get('/check', (req, res) => {
-  res.json({
-    status: !!req.cookies.recognizeRecruiter,
-  });
+router.get('/', checkIsAdmin, async (req, res) => {
+  try {
+    const [recruiters] = await pool.query(
+      'SELECT id, name, email, telephone, visits FROM recruiter',
+    );
+    return res.json(recruiters);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+router.get('/check', async (req, res) => {
+  const { recruiter } = req.cookies;
+  if (!recruiter) return res.json({ status: false });
+  try {
+    await jwt.verify(recruiter, process.env.JWT_SECRET);
+    return res.json({
+      status: true,
+    });
+  } catch (err) {
+    console.error(err);
+    res.clearCookie('recruiter');
+    return res.json({ status: false });
+  }
 });
 
 module.exports = router;
